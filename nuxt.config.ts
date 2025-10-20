4a1dea8ffbd9fd33a4cf3b9ce54c764ef96aba41
ZachZQY <zhangquanyinhahaha@163.com>
2025-10-20 09:27:12 +0800

    添加 Facebook Meta Pixel 代码以支持页面跟踪和分析，增强网站的营销能力。

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -72,7 +72,24 @@ export default defineNuxtConfig({
           gtag('config', 'G-H03SG1NBFP');
                         `,
         },
+        // Facebook Meta Pixel Code
+        {
+          innerHTML: `!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '795952583356142');
+fbq('track', 'PageView');`,
+        },
 
+      ],
+      noscript: [
+        // Facebook Meta Pixel noscript
+        { innerHTML: '<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=795952583356142&ev=PageView&noscript=1" />' },
       ],
 
     },
