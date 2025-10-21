8be4d1f29e5c122606caa6b38e5b52a13a67732d
ZachZQY <zhangquanyinhahaha@163.com>
2025-10-21 08:33:11 +0800

    添加 Facebook Meta Pixel 代码以跟踪提交申请事件，增强网站的营销分析能力。

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -83,7 +83,9 @@ t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
 fbq('init', '795952583356142');
-fbq('track', 'PageView');`,
+fbq('track', 'PageView');
+fbq('track', 'SubmitApplication');
+`,
         },
 
       ],
