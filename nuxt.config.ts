bdc23a00f20b5f7d8a2216cdd2d0bd3907ddb048
yundasurrogacy <leadtopmediahk034@gmail.com>
2026-04-20 11:17:48 +0800

    ads代码安装

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -218,7 +218,7 @@ export default defineNuxtConfig({
               function loadAnalytics() {
                 // Google Tag Manager
                 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-W6MHCNTV');
-                // Google Analytics
+                // Google Analytics + Google Ads
                 var gtagScript = document.createElement('script');
                 gtagScript.async = true;
                 gtagScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-H03SG1NBFP';
@@ -228,6 +228,7 @@ export default defineNuxtConfig({
                   function gtag(){dataLayer.push(arguments);}
                   gtag('js', new Date());
                   gtag('config', 'G-H03SG1NBFP');
+                  gtag('config', 'AW-17913743847');
                 };
               }
               if ('requestIdleCallback' in window) {
