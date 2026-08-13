17ab2fe86f68b960281f03a0bf175ab370341c08
yundasurrogacy <leadtopmediahk034@gmail.com>
2026-08-13 17:16:37 +0800

    fix(analytics): keep GA4 under a single GTM owner

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -379,16 +379,15 @@ export default defineNuxtConfig({
                 document.head.appendChild(ahrefsScript);
                 // Google Tag Manager
                 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-W6MHCNTV');
-                // Google Analytics + Google Ads
+                // Google Ads only. GA4 is configured by GTM so it has a single owner.
                 var gtagScript = document.createElement('script');
                 gtagScript.async = true;
-                gtagScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-H03SG1NBFP';
+                gtagScript.src = 'https://www.googletagmanager.com/gtag/js?id=AW-17913743847';
                 document.head.appendChild(gtagScript);
                 gtagScript.onload = function() {
                   window.dataLayer = window.dataLayer || [];
                   function gtag(){dataLayer.push(arguments);}
                   gtag('js', new Date());
-                  gtag('config', 'G-H03SG1NBFP');
                   gtag('config', 'AW-17913743847');
                 };
               }
